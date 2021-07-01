43d34e2c85bdb19d5c23d709e3e3f48f4f565256
peeraya <peeraya_boon@cmu.ac.th>
2021-07-01 23:24:49 +0700

    css

--- project.js
+++ project.js
@@ -59,9 +59,13 @@ function addAnimeToCard(anime) {
   output.appendChild(card);
 }
 
-document.getElementById("searchButton").addEventListener("click", function(){searchAnime()});
+document.getElementById("searchButton").addEventListener("click", function () {
+  searchAnime();
+});
 
-document.getElementById("mylist").addEventListener("click", function(){showMyList()});
+document.getElementById("mylist").addEventListener("click", function () {
+  showMyList();
+});
 function showMyList() {
   output.innerHTML = "";
   fetch(`https://se104-project-backend.du.r.appspot.com/movies/632110349`)
@@ -114,76 +118,78 @@ function addcardOnMylist(anime) {
   output.appendChild(card);
 }
 function showcontent(anime) {
-    let overAll = document.createElement("div");
-    overAll.classList.add("row");
-    let Allmight = document.createElement("div");
-    Allmight.classList.add("col-3");
+  let overAll = document.createElement("div");
+  overAll.classList.add("row");
+  overAll.classList.add("flex-container");
+  let Allmight = document.createElement("div");
+  Allmight.classList.add("col-3");
 
-    let one = document.createElement("div");
-    one.classList.add("card");
+  let one = document.createElement("div");
+  one.classList.add("card");
 
-    let img = document.createElement("img");
-    img.classList.add("card-img-top");
-    let imgname = data.image_url;
-    img.setAttribute("src", imgname);
+  let img = document.createElement("img");
+  img.classList.add("card-img-top");
+  let imgname = data.image_url;
+  img.setAttribute("src", imgname);
 
-    let inone = document.createElement("div");
-    inone.classList.add("card-body");
-    let H5 = document.createElement("h5");
-    H5.classList.add("card-title");
-    let name = data.title;
-    H5.innerHTML = name;
+  let inone = document.createElement("div");
+  inone.classList.add("card-body");
+  let H5 = document.createElement("h5");
+  H5.classList.add("card-title");
+  let name = data.title;
+  H5.innerHTML = name;
 
-    one.appendChild(img);
-    Allmight.appendChild(one);
-    let txtName = document.createElement("div");
-    txtName.classList.add("col-9");
-    let url = data.url;
-    let title = data.title;
-    let synopsis = data.synopsis;
-    let type = data.type;
-    let episodes = data.episodes;
-    let score = data.score;
-    let rated = data.rated;
+  one.appendChild(img);
+  Allmight.appendChild(one);
+  let txtName = document.createElement("div");
+  txtName.classList.add("col-9");
+  let url = data.url;
+  let title = data.title;
+  let synopsis = data.synopsis;
+  let type = data.type;
+  let episodes = data.episodes;
+  let score = data.score;
+  let rated = data.rated;
 
-    let row1 = document.createElement("div");
-    row1.classList.add("row");
-    col_item = document.createElement("div")
-    col_item.classList.add("col")
-    col_item.innerHTML = ` Name : ${title} <br>
+  let row1 = document.createElement("div");
+  row1.classList.add("row");
+  row1.classList.add("flex-container");
+  col_item = document.createElement("div");
+  col_item.classList.add("col");
+  col_item.innerHTML = ` Name : ${title} <br>
                               Type : ${type} <br>
                               Episodes : ${episodes} <br>
                               Rated : ${rated} <br>
                               Score : ${score} <br>
                               Url : ${url} <br>
                               ${synopsis}`;
-    row1.appendChild(col_item)
-    let row2 = document.createElement("div");
-    row2.classList.add("row");
-    let col10 = document.createElement("div");
-    col10.classList.add("col-10");
-    let col2 = document.createElement("div");
-    col2.classList.add("col-2");
-    let button = document.createElement("button");
-    button.classList.add("btn");
-    button.classList.add("btn-success");
-    button.setAttribute("type", "button");
-    button.innerText = "Back";
-    button.addEventListener("click", function () {
-      output.innerHTML = "";
-      showMyList()
-    });
+  row1.appendChild(col_item);
+  let row2 = document.createElement("div");
+  row2.classList.add("row");
+  let col10 = document.createElement("div");
+  col10.classList.add("col-10");
+  let col2 = document.createElement("div");
+  col2.classList.add("col-2");
+  let button = document.createElement("button");
+  button.classList.add("btn");
+  button.classList.add("btn-success");
+  button.setAttribute("type", "button");
+  button.innerText = "Back";
+  button.addEventListener("click", function () {
+    output.innerHTML = "";
+    showMyList();
+  });
 
-    col2.appendChild(button);
-    row2.appendChild(col10);
-    row2.appendChild(col2);
-    txtName.appendChild(row1);
-    txtName.appendChild(row2);
+  col2.appendChild(button);
+  row2.appendChild(col10);
+  row2.appendChild(col2);
+  txtName.appendChild(row1);
+  txtName.appendChild(row2);
 
-    overAll.appendChild(Allmight);
-    overAll.appendChild(txtName);
-    output.appendChild(overAll);
-  }
+  overAll.appendChild(Allmight);
+  overAll.appendChild(txtName);
+  output.appendChild(overAll);
+}
 function deleteData(id) {
   fetch(
     `https://se104-project-backend.du.r.appspot.com/movie?id=632110349&&movieId=${id.id}`,
@@ -203,4 +209,5 @@ function deleteData(id) {
     })
     .catch((error) => {
       alert(`your input anime id is not in the database`);
-    })}
+    });
+}
